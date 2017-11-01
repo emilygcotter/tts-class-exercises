a7d77163f4b45e457af62a8c95dab1b85ecf1a00
EMILYCOTTER <EMILY.G.COTTER@GMAIL.COM>
2017-11-01 14:10:22 -0400

    added playTournament function

--- rps.js
+++ rps.js
@@ -16,6 +16,22 @@ var player2 = {
               }
             };
 
+var player3 = {
+              name: 'jane',
+              score: 0,
+              hand: function() {
+                return hands[parseInt(Math.random()*10)%3];
+              }
+            };
+
+var player4 = {
+              name: 'emily',
+              score: 0,
+              hand: function() {
+                return hands[parseInt(Math.random()*10)%3];
+              }
+            };
+
 playGame(player1, player2, 7);
 
 function playRound(playOne, playTwo) {
@@ -59,3 +75,31 @@ function playGame(firstPlayer, secondPlayer, playUntil) {
     console.log("Tie!");
     }
 };
+
+function playTournament(firstPlayer, secondPlayer, thirdPlayer, fourthPlayer, playUntil) {
+    playGame(player1, player2, 3);
+    playGame(player3, player4, 3);
+    if (player1.score > player2.score && player3.score > player4.score) {
+      playGame(player1, player3, 3);
+    } else if (player2.score > player1.score && player4.score > player3.score) {
+      playGame(player2, player4, 3);
+    } else {
+      console.log("try again");
+    };
+    if (player1.score > player3.score) {
+      console.log("The world champion is " + player1.name + "!")
+    } else if (player3.score > player1.score) {
+      console.log("The world champion is " + player3.name + "!")
+    } else {
+      console.log("Keep playing!");
+    };
+    if (player2.score > player4.score) {
+      console.log("The world champion is " + player2.name + "!")
+    } else if (player4.score > player2.score) {
+      console.log("The world champion is " + player4.name + "!")
+    } else {
+      console.log("Keep playing!");
+    };
+}
+
+playTournament(player1, player2, player3, player4, 3);
